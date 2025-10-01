7a42b4daa6b4d484d391ba07ee13891c7d1b8617
ecole1rllco <erika@rllco.com>
2025-10-01 16:22:04 -0400

    Update OnChange for Status

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -17,6 +17,10 @@ export const Contact = () => {
     const [errors, setErrors] = useState({});
 
     const onFormUpdate = (category, value) => {
+        //Clear the status message when the user starts typing
+        setStatus({});
+
+        // Clear the error for the specific field being updated
         setFormDetails({
             ...formDetails,
             [category]: value
@@ -96,16 +100,16 @@ export const Contact = () => {
                         <form onSubmit={handleSubmit}>
                             <Row>
                                 <Col sm={12} className="px-1">
-                                    <input type="text" value={formDetails.fullname} placeholder="Full Name" maxLength="150" onClick={(e) => setStatus({})} onChange={(e) => onFormUpdate('fullname', e.target.value)} className={errors.fullname ? 'input-error' : ''} />                                   
+                                    <input type="text" value={formDetails.fullname} placeholder="Full Name" maxLength="150" onChange={(e) => onFormUpdate('fullname', e.target.value)} className={errors.fullname ? 'input-error' : ''} />                                   
                                 </Col>
                                 <Col sm={12} className="px-1">
-                                    <input type="email" value={formDetails.email} placeholder="Email" maxLength="150" onClick={(e) => setStatus({})} onChange={(e) => onFormUpdate('email', e.target.value)} className={errors.email ? 'input-error' : ''} />                                   
+                                    <input type="email" value={formDetails.email} placeholder="Email" maxLength="150" onChange={(e) => onFormUpdate('email', e.target.value)} className={errors.email ? 'input-error' : ''} />                                   
                                 </Col>
                                 <Col sm={12} className="px-1">
-                                    <input type="tel" value={formDetails.phone} placeholder="Phone Number" maxLength="15" onClick={(e) => setStatus({})} onChange={(e) => onFormUpdate('phone', e.target.value)} className={errors.phone ? 'input-error' : ''} />         
+                                    <input type="tel" value={formDetails.phone} placeholder="Phone Number" maxLength="15" onChange={(e) => onFormUpdate('phone', e.target.value)} className={errors.phone ? 'input-error' : ''} />         
                                 </Col>
                                 <Col sm={12} className="px-1">
-                                    <textarea rows="6" value={formDetails.message} placeholder="Message" maxLength="1000" onClick={(e) => setStatus({})} onChange={(e) => onFormUpdate('message', e.target.value)} className={errors.message ? 'input-error' : ''}></textarea>                                  
+                                    <textarea rows="6" value={formDetails.message} placeholder="Message" maxLength="1000" onChange={(e) => onFormUpdate('message', e.target.value)} className={errors.message ? 'input-error' : ''}></textarea>                                  
                                 </Col>
                                 <Col sm={12} className="px-1 d-flex align-items-center">
                                     <button type="submit" className={buttonAnimation ? "submitting" : ""}>
